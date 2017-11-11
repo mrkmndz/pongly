5b60af0ae317e50a3db611617a1f495190b78318
Mark Mendoza <mendoza.mark.a@gmail.com>
2017-11-11 04:48:29 -0800

    movement

--- letterhack.ino
+++ letterhack.ino
@@ -12,11 +12,19 @@ typedef struct ball_state_struct {
   vec_t velocity;
 } ball_state_t;
 
+typedef struct player_state_struct {
+  byte position;
+  byte last_position;
+  byte direction;
+  unsigned long next_sense;
+} player_state_t;
+  
+
 typedef struct game_state_struct {
   ball_state_t ball_state;
   byte paddle_lengths;
-  byte p1_position;
-  byte p2_position;
+  player_state_t p1_state;
+  player_state_t p2_state;
   bool finished;
 } game_state_t;
 
@@ -39,6 +47,14 @@ void set_pins_high(int *pins, size_t len) {
   }
 }
 
+player_state_struct get_init_player() {
+  player_state_struct pstate;
+  pstate.position = 0;
+  pstate.last_position = 0
+  pstate.direction = 0;
+  pstate.next_sense = millis();
+}
+
 game_state_t state;
 void setup() {
   pinMode(A3, INPUT);
@@ -53,8 +69,8 @@ void setup() {
   state.ball_state.position.y = 4;
   state.ball_state.velocity.x = 0;
   state.ball_state.velocity.y = 1;
-  state.p1_position = 0;
-  state.p2_position = 0;
+  state.p1_state = get_init_player();
+  state.p2_state = get_init_player();
   state.paddle_lengths = 2;
   state.finished = false;
 
@@ -134,39 +150,36 @@ game_state_t move_ball(game_state_t state) {
   return state;
 }
 
-bool blocked_by_player(game_state_t state) {
-  byte y = state.ball_state.position.y;
-  byte paddle_pos = (y == 0) ? state.p1_position : state.p2_position;
-  byte x = state.ball_state.position.x;
-  return x >= paddle_pos && x < paddle_pos + state.paddle_lengths;
+bool blocked_by_player(game_state_t *state) {
+  byte y = state->ball_state.position.y;
+  player_state_t target = (y == 0) ? state->p1_state : state->p2_state;
+  byte paddle_pos = target.position;
+  byte x = state->ball_state.position.x;
+  if (x < paddle_pos || x >= paddle_pos + state.paddle_lengths) {
+    return false;
+  }
+  state->ball_state->velocity.y *= -1;
+  state.ball_state.velocity.x = target.direction;
 }
 
 game_state_t proceed(game_state_t state) {
   state = move_ball(state);
 
-  switch (state.ball_state.position.x) {
-    case 0:
-    case 5:
-        state.ball_state.velocity.x *= -1;
-    break;
+  byte x = state.ball_state.position.x;
+  if (x < 0) {
+    state.ball_state.velocity.x = 1;
+    state.ball_state.position.x = 0;
+  if (x > 5) {
+    state.ball_state.velocity = -1;
+    state.ball_state.position.x = 5;
   }
 
   switch (state.ball_state.position.y) {
     case 0:
-      if (blocked_by_player(state)) {
-        state.ball_state.velocity.y *= -1;
-      } else {
-        Serial.println("done");
-        state.finished = true;
-      }
-    break;
     case 5:
-      if (blocked_by_player(state)) {
-        state.ball_state.velocity.y *= -1;
-      } else {
-        state.finished = true;
-      }
-    break;
+    if (!blocked_by_player(&state)) {
+      state.finished = true;
+    }
   }
 
   return state;
@@ -190,18 +203,29 @@ void print_state(game_state_t state) {
   display(pattern);
 }
 
-byte get_pos(int pin, byte paddle_size) {
+void update_player(player_state_t *state) {
   float val = analogRead(pin);    // read the input pin
   float max_val = 400;
   int range = 6 - paddle_size;
   int pos = (val/max_val) * range;
-  return (pos > range) ? range : pos;
+  state->position = (pos > range) ? range : pos;
+  if (millis() > state->next_sense) {
+    state->next_sense += 10;
+    if (state->position == state->last_position) {
+      state->direction = 0;
+    } else if (state->position > state->last_positon) {
+      state->direction = 1;
+    } else {
+      state->direction = -1;
+    }
+    state->last_position = state->position;
+  }
 }
 
 void loop() {
   static unsigned long next_frame = millis();
-  state.p1_position = get_pos(A3, state.paddle_lengths);
-  state.p2_position = get_pos(A0, state.paddle_lengths);
+  update_player(&state->p1_state);
+  update_player(&state->p2_state);
   if (millis() > next_frame && !state.finished) {
     next_frame += 100;
     state = proceed(state);
